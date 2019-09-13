6d1512e53e770357dd39bd4609381ceb83b293ad
Yoel Lev <yoel@tinytap.it>
2019-09-13 15:07:18 +0300

    Initial Commit

---
+++ AppleSignInExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppleSignInExample
+//
+//  Created by Yoel Lev on 13/09/2019.
+//  Copyright © 2019 Yoel Lev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
